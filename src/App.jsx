98cd2f81089fc759f8c595d3a2b369c80eff683d
YakubuAmidu <yakubu.amidu@yahoo.com>
2024-04-27 10:50:29 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import "./App.css";
 
-import { Container } from "./css/Container";
+import { Container, Maincontainer } from "./css/Container";
 
 import {
   LeftContainer,
@@ -33,81 +33,83 @@ import {
 
 const App = () => {
   return (
-    <Container>
-      <LeftContainer>
-        <LeftSection>
-          <Name>Yakubu Amidu</Name>
-          <Contact>
-            <Phone>Phone: </Phone>
-            <p>323-555-5555</p>
-          </Contact>
-          <Email>
-            <Mail>Email: </Mail>
-            <p>yakubamidu@gmail.com</p>
-          </Email>
-          <Location>
-            <Address>Location: </Address>
-            <p>Los Angele, California</p>
-          </Location>
-        </LeftSection>
-      </LeftContainer>
+    <Maincontainer>
+      <Container>
+        <LeftContainer>
+          <LeftSection>
+            <Name>Yakubu Amidu</Name>
+            <Contact>
+              <Phone>Phone: </Phone>
+              <p>323-555-5555</p>
+            </Contact>
+            <Email>
+              <Mail>Email: </Mail>
+              <p>yakubamidu@gmail.com</p>
+            </Email>
+            <Location>
+              <Address>Location: </Address>
+              <p>Los Angele, California</p>
+            </Location>
+          </LeftSection>
+        </LeftContainer>
 
-      <RightContainer>
-        <div>
-          <Professionalsummary>Professional summary</Professionalsummary>
-          <Summaryparagraph>
-            Aspiring and detail-oriented Full-stack Software Developer with a
-            strong foundation in front-end and back-end technologies. Eager to
-            contribute my coding skills and passion for creating innovative
-            solutions in an entry-level role that fosters professional growth
-            and allows me to make a meaningful impact.
-          </Summaryparagraph>
-        </div>
-        <div>
-          <Skills>Skills</Skills>
-          <Skillsparagraph>
-            <Unorderedlistcontainer>
-              <Unorderedlistmain>
-                <UnorderedlistTitle>FRONT-END</UnorderedlistTitle>
-                <li>Html5 & css</li>
-                <li>Reactjs</li>
-                <li>React-native</li>
-                <li>Bootstrap & media query</li>
-                <li>Node-sass & styled-components</li>
-              </Unorderedlistmain>
-              <ul>
-                <UnorderedlistTitle>BACK-END</UnorderedlistTitle>
-                <li>Javascript</li>
-                <li>Express</li>
-                <li>Mongoose</li>
-                <li>Mongodb</li>
-                <li>Firebase</li>
-              </ul>
-            </Unorderedlistcontainer>
-          </Skillsparagraph>
-        </div>
-        <div>
-          <Experience>Experience</Experience>
-          <Experienceparagraph>
-            <p>
-              Freelancer since 2022. Turning my passion into a profession after
-              completing my courses.
-            </p>
-          </Experienceparagraph>
-        </div>
-        <div>
-          <Education>Education</Education>
-          <Educationpargraph>
-            <p>Los Angeles City College</p>
-            <p>Udemy.com online courses</p>
-          </Educationpargraph>
-        </div>
-        <div>
-          <Language>Language</Language>
-          <Languagepargraph>English</Languagepargraph>
-        </div>
-      </RightContainer>
-    </Container>
+        <RightContainer>
+          <div>
+            <Professionalsummary>Professional summary</Professionalsummary>
+            <Summaryparagraph>
+              Aspiring and detail-oriented Full-stack Software Developer with a
+              strong foundation in front-end and back-end technologies. Eager to
+              contribute my coding skills and passion for creating innovative
+              solutions in an entry-level role that fosters professional growth
+              and allows me to make a meaningful impact.
+            </Summaryparagraph>
+          </div>
+          <div>
+            <Skills>Skills</Skills>
+            <Skillsparagraph>
+              <Unorderedlistcontainer>
+                <Unorderedlistmain>
+                  <UnorderedlistTitle>FRONT-END</UnorderedlistTitle>
+                  <li>Html5 & css</li>
+                  <li>Reactjs</li>
+                  <li>React-native</li>
+                  <li>Bootstrap & media query</li>
+                  <li>Node-sass & styled-components</li>
+                </Unorderedlistmain>
+                <ul>
+                  <UnorderedlistTitle>BACK-END</UnorderedlistTitle>
+                  <li>Javascript</li>
+                  <li>Express</li>
+                  <li>Mongoose</li>
+                  <li>Mongodb</li>
+                  <li>Firebase</li>
+                </ul>
+              </Unorderedlistcontainer>
+            </Skillsparagraph>
+          </div>
+          <div>
+            <Experience>Experience</Experience>
+            <Experienceparagraph>
+              <p>
+                Freelancer since 2022. Turning my passion into a profession
+                after completing my courses.
+              </p>
+            </Experienceparagraph>
+          </div>
+          <div>
+            <Education>Education</Education>
+            <Educationpargraph>
+              <p>Los Angeles City College</p>
+              <p>Udemy.com online courses</p>
+            </Educationpargraph>
+          </div>
+          <div>
+            <Language>Language</Language>
+            <Languagepargraph>English</Languagepargraph>
+          </div>
+        </RightContainer>
+      </Container>
+    </Maincontainer>
   );
 };
 
--- src/css/Container.js
+++ src/css/Container.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const Maincontainer = styled.div`
+  padding: 4rem 0rem;
+`;
+
 export const Container = styled.div`
   max-width: 1000px;
   margin: 0 auto;
